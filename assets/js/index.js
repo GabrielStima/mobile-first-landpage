0437224da341f73d2b1d7a0c06caa5bdb3fd0696
Gabriel Rocha Stimamiglio <gabrielstimamiglio@gmail.com>
2025-02-14 18:50:10 -0300

    Finalize the header and start developing responsiveness

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function closeMenu() {
+    document.getElementById("open-menu").style.display = "none";
+}
+
+function openMenu() {
+    document.getElementById("open-menu").style.display = "flex";
+}
